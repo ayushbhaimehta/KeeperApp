1ca3fee352cd2f6c09cb7802dd5e4c29e5027186
unknown <ayushbhaimehta20002@gmail.com>
2022-06-02 02:52:55 +0530

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>
+                Title of you note.
+            </h1>
+
+            <p>
+                content of your note.
+            </p>
+        </div>
+    );
+};
+
+export default Note; 
